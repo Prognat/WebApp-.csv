2eebfa39b150937ed5b3b471b31a74bcb03af8b1
Lages Nathan <lages.nathan@fronius.com>
2025-08-05 08:46:49 +0200

    Placeholder für die Daten des Plots, Plot erstellt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from bokeh.plotting import figure, show
+from bokeh.plotting import figure
+from bokeh.models import ColumnDataSource
 
 # Anzahl an Gleitkommastellen werden auf 15 gesetzt
 pd.set_option('display.precision', 15)
@@ -26,6 +27,11 @@ def load_data(file_path):
     data_str = "\n".join(lines[header_index:]) # Verwandelt die Daten in einen String bsp: "TIME,CH1\n1,1\n..."
     df = pd.read_csv(StringIO(data_str))
     return df
+
+source = ColumnDataSource(data=dict(x=[], y=[])) # Hier werden die Daten für das Plot gespeichert (Welche Nummern auf den Achsen sind)
+
+plot = figure(title="CSV Data Plot", height=300, width=300)
+plot.line('x', 'y', source=source, line_width=2) # Daten von source werden als Linie geplottet
 
 df = load_data('Ex1_HP_Diff.csv')
 print(df)
